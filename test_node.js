c85c36e2dca6ac88ba8f5ed0c3818e7c8d4a53d8
xin.chen <18017657713@163.com>
2023-04-06 13:54:50 +0800

    12

--- test_node.js
+++ test_node.js
@@ -8,9 +8,9 @@ http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
 
     // 发送响应数据 "Hello World"
-    response.end('Hello World ,' +
+    response.end('Hello World12 ,' +
         '构建成功\n');
 }).listen(8086);
 
 // 终端打印如下信息
-console.log('1 2 3 4 5 6 7 8 9 Server running at http://127.0.0.1:8086/');
+console.log('1 2 3 4 5 6 7 8 9 12 Server running at http://127.0.0.1:8086/');
